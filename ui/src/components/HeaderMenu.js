f2efbab37a4e2306406fce4d2a04ca1881519504
Peter <peter@thundercore.tw>
2021-12-07 15:50:24 +0800

    update css

--- ui/src/components/HeaderMenu.js
+++ ui/src/components/HeaderMenu.js
@@ -1,9 +1,9 @@
-import React from "react"
-import { default as MenuItems } from "./MenuItems"
-import { Wallet } from "./Wallet"
+import React from "react";
+import { default as MenuItems } from "./MenuItems";
+import { Wallet } from "./Wallet";
 
-import { bridgeType } from "../stores/utils/bridgeMode"
-import { FormattedMessage } from "react-intl"
+import { bridgeType } from "../stores/utils/bridgeMode";
+import { FormattedMessage } from "react-intl";
 
 export const HeaderMenu = () => (
   <div className="header-menu">
@@ -24,20 +24,14 @@ export const HeaderMenu = () => (
       </span>
     </a>
     <a
-      href={`${
-        bridgeType === "eth"
-          ? "https://thundercore.zendesk.com/hc/en-us/articles/4402401778841-ThunderCore-Bridge-Transferring-into-ThunderCore-from-Ethereum"
-          : bridgeType === "bsc"
-          ? "https://thundercore.zendesk.com/hc/en-us/articles/4402401303577-ThunderCore-Bridge-Transferring-into-ThunderCore-from-Binance-Smart-Chain-"
-          : "https://thundercore.zendesk.com/hc/en-us/articles/4402658518169"
-      }`}
+      href="https://docs.thundercore.com/docs/Thunder-Bridge-audit-en.pdf"
       target="_blank"
       className="menu-items"
     >
       <span className="menu-items-text">
-        <FormattedMessage id="components.i18n.HeaderMenu.tutorial" />
+        <FormattedMessage id="components.i18n.HeaderMenu.audit" />
       </span>
     </a>
     <Wallet />
   </div>
-)
+);
