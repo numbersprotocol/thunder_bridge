5d23914b1faf301607763eaabad9b0613ff92a69
Benson Cho <benson.cho@thundercore.tw>
2022-04-15 16:55:11 +0800

    APPS-2815 rebranding

--- ui/src/components/MenuItems.js
+++ ui/src/components/MenuItems.js
@@ -1,9 +1,10 @@
-import React from "react"
-import { /*EventsIcon, */ StatusIcon, StatisticsIcon } from "./menu-icons"
-import { Link } from "react-router-dom"
-import { injectIntl } from "react-intl"
+import React from "react";
+import { /*EventsIcon, */ StatusIcon, StatisticsIcon } from "./menu-icons";
+import { Link, useHistory } from "react-router-dom";
+import { injectIntl } from "react-intl";
 
 const MenuItems = ({ intl }) => {
+  let history = useHistory();
   const menuItems = [
     /*{
       hide: withoutEvents,
@@ -25,15 +26,21 @@ const MenuItems = ({ intl }) => {
         id: "components.i18n.MenuItems.statistics",
       }),
     },
-  ]
-
+  ];
   return menuItems.map((item, index) => {
+    const isActive =
+      !!history.pathname && history.pathname.indexOf(item.link) != -1;
+    console.log(`isActive ${isActive} item.link  ${item.link}`);
     return (
-      <Link key={index} to={item.link} className="menu-items">
+      <Link
+        key={index}
+        to={item.link}
+        className={`menu-items ${isActive ? "active" : ""}`}
+      >
         <span className="menu-items-text">{item.text}</span>
       </Link>
-    )
-  })
-}
+    );
+  });
+};
 
-export default injectIntl(MenuItems)
+export default injectIntl(MenuItems);
--- ui/src/components/icons/InfoIcon.js
+++ ui/src/components/icons/InfoIcon.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export const InfoIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <path
-      fillRule="evenodd"
-      d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-22C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 16a1 1 0 0 1-1-1v-6a1 1 0 0 1 2 0v6a1 1 0 0 1-1 1zm0-10a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
+      d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+      stroke="#0844D2"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12 16V12"
+      stroke="#0844D2"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12 8H12.01"
+      stroke="#0844D2"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
-)
+);
