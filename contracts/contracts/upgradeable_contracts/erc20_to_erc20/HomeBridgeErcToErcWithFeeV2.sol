7ae50f412fdca0f9d33cae729b45660ae087999d
waynetseng <wayne.tseng@thundercore.tw>
2022-08-24 12:00:18 +0800

    Use eternal storage to storage MinterBurner contract address

--- contracts/contracts/upgradeable_contracts/erc20_to_erc20/HomeBridgeErcToErcWithFeeV2.sol
+++ contracts/contracts/upgradeable_contracts/erc20_to_erc20/HomeBridgeErcToErcWithFeeV2.sol
@@ -5,10 +5,12 @@ import "./HomeBridgeErcToErcWithFee.sol";
 import "../../IMinterBurner.sol";
 
 contract HomeBridgeErcToErcWithFeeV2 is HomeBridgeErcToErcWithFee {
-    IMinterBurner public minterBurner;
-
     function setMinterBurner(IMinterBurner _minterBurner) external onlyOwner {
-        minterBurner = _minterBurner;
+        addressStorage[keccak256(abi.encodePacked("minterBurner"))] = _minterBurner;
+    }
+
+    function minterBurner() public returns (IMinterBurner) {
+        return IMinterBurner(addressStorage[keccak256(abi.encodePacked("minterBurner"))]);
     }
 
     function onTokenTransfer(address _from, uint256 _value, bytes _data) external returns(bool) {
@@ -22,7 +24,7 @@ contract HomeBridgeErcToErcWithFeeV2 is HomeBridgeErcToErcWithFee {
         uint256 value = _value.sub(fee);
 
         setTotalSpentPerDay(getCurrentDay(), totalSpentPerDay(getCurrentDay()).add(value));
-        minterBurner.burn(address(this), value);
+        minterBurner().burn(address(this), value);
         fireEventOnTokenTransfer(_from, value);
         return true;
     }
@@ -32,17 +34,17 @@ contract HomeBridgeErcToErcWithFeeV2 is HomeBridgeErcToErcWithFee {
 
         uint256 fee = depositFee(_value);
         if (_value <= fee) {
-            minterBurner.mint(feeReceiver(), _value);
+            minterBurner().mint(feeReceiver(), _value);
             return true;
         }
 
         if (fee != 0) {
-            minterBurner.mint(feeReceiver(), fee);
+            minterBurner().mint(feeReceiver(), fee);
         }
 
         uint256 value = _value.sub(fee);
         if (value != 0) {
-            minterBurner.mint(_recipient, value);
+            minterBurner().mint(_recipient, value);
         }
 
         return true;
