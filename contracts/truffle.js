a394caf4ab6ff286538d44792b6a6782b07513fc
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-02-02 22:31:06 +0800

    build: add goerli to truffle config
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- contracts/truffle.js
+++ contracts/truffle.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const fs = require("fs");
 const JSON5 = require("json5");
 
@@ -79,29 +79,6 @@ module.exports = {
       network_id: 1,
       gasPrice: 1000000000
     },
-    rinkeby: {
-      provider: () => {
-        if (privateKeys === null) {
-          throw new Error("Create a .private-keys file");
-        }
-        if (infuraProjectId === null) {
-          throw new Error('Set "infura_project_id" in local.jsonc');
-        }
-        if (infuraProjectSecret === null) {
-          throw new Error('Set "infura_project_secret" in local.jsonc');
-        }
-        // FIXME: infuraProjectSecret not used yet
-        return new HDWalletProvider(
-          privateKeys,
-          `https://rinkeby.infura.io/v3/${infuraProjectId}`,
-          0 /*address_index*/,
-          privateKeys.length /*num_address*/
-        );
-      },
-      network_id: 4,
-      gas: 4700000,
-      gasPrice: 80000000000
-    },
     kovan: {
       provider: () => {
         if (privateKeys === null) {
@@ -124,21 +101,29 @@ module.exports = {
       network_id: 42,
       gasPrice: 1000000000
     },
-    "bsc-testnet": {
+    goerli: {
       provider: () => {
         if (privateKeys === null) {
           throw new Error("Create a .private-keys file");
         }
+        if (infuraProjectId === null) {
+          throw new Error('Set "infura_project_id" in local.jsonc');
+        }
+        if (infuraProjectSecret === null) {
+          throw new Error('Set "infura_project_secret" in local.jsonc');
+        }
+        // FIXME: infuraProjectSecret not used yet
         return new HDWalletProvider(
           privateKeys,
-          `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+          `https://goerli.infura.io/v3/${infuraProjectId}`,
           0 /*address_index*/,
           privateKeys.length /*num_address*/
         );
       },
-      network_id: 97,
-      gasPrice: 10000000000
+      network_id: 5,
+      gasPrice: 5000000000
     },
+
     core: {
       host: "localhost",
       port: "8777",
